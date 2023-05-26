26653b3402fc7fd31acfa4cc12aee79c68b03e09
Stephanie Gonzalez <fanystephanie08@hotmail.com>
2023-05-26 10:21:08 -0700

    Se agrego un SweetAlert para SummaryOrder. UI Fase 5

--- js/carSelection.js
+++ js/carSelection.js
@@ -38,4 +38,19 @@ const cities = {
     'Ensenada' : 'ENS'
 }
 
+$('#btn_summary').click(function(){
+    Swal.fire({
+        icon: 'warning',
+        title: 'Summary Order!',
+        html: '<b>Limited mileage</b> : Included <br> <b>Basic Protection Package</b>: Included <br><br> <h4 style="font-weight: bold">TOTAL PRICE:</h4> $10,000',
+        confirmButtonText: 'Rent a car',
+        // imageUrl: 'imagesCar/CSS2010G1.png',
+        // imageWidth: 400,
+        // imageHeight: 200,
+        // imageAlt: 'Custom image'
+        footer: '<span style="color: red"> Please check the order in detail before renting</span>'
+    })
+});
+
+
 searchCar(locationEntered);
