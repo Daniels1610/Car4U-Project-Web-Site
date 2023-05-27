7071342eb0d717c3d5d21a39e9576bb0faf984f0
Daniels1610 <danielagraz1610@hotmail.com>
2023-05-26 19:25:05 -0700

    Add DropoffDateValidation to verify that a DropOff Date hasn't expired

--- js/carSearch.js
+++ js/carSearch.js
@@ -1,15 +1,19 @@
 const searchBtn = document.querySelector(".search-btn");
-const locationInput = document.getElementById("location").value;
-const pickupInput = document.querySelector(".pickup-Date").value;
-const dropoffInput = document.querySelector(".dropoff-Date").value;
+const locationInput = document.getElementById("location");
+const pickupInput = document.querySelector(".pickup-Date"); 
+const dropoffInput = document.querySelector(".dropoff-Date");
 let timerInterval;
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (!pickupInput | !dropoffInput) {
-        dateAlert();
-        return;
+    if (!pickupInput.value | !dropoffInput.value) {
+        dateAlert(); return;
     }
+
+    if(!dropoffDateValidation(pickupInput.value, dropoffInput.value)) {
+        dateExpiredAlert(); return;
+    } 
+
     localStorage.setItem('location', locationInput);
     localStorage.setItem('pickupDate', pickupInput);
     localStorage.setItem('dropOff', dropoffInput);
@@ -45,3 +49,26 @@ const dateAlert = () => {
         'question'
       )
 };
+
+const dateExpiredAlert = () => {
+    Swal.fire(
+        'Invalid Drop-Off Date',
+        'Make sure to enter a valid<br>' + 
+        'Drop-Off Date',
+        'question'
+      )
+};
+
+const dropoffDateValidation = (firstDate, secondDate) =>{
+  firstDate = new Date(reformatDate(firstDate));
+  secondDate = new Date(reformatDate(secondDate));
+  if(firstDate.setHours(0, 0, 0, 0) <= secondDate.setHours(0, 0, 0, 0)) {
+    return true;
+  } else {return false;}
+}; 
+
+const reformatDate = (dateString) => {
+    var dateParts = dateString.split("/"); var month = dateParts[0];
+    var day = dateParts[1]; var year = dateParts[2];
+    return year + "-" + ("0" + month).slice(-2) + "-" + ("0" + day).slice(-2);
+}
