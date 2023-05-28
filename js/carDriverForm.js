2e0fefb3ef7bb75e3d49cb4d43c72fbb25d89c39
Daniels1610 <danielagraz1610@hotmail.com>
2023-05-28 13:41:12 -0700

    Retrieve all values from Driver Form in a JS Object and store them in LocalStorage to received them in Checkout.html

--- js/carDriverForm.js
+++ js/carDriverForm.js
@@ -3,4 +3,29 @@ const carImage = document.querySelector('.car-image');
 
 carImage.src = `imagesCar/${selectedCarID}.png`;
 
+// DRIVER INFORMATION
+const driverInformation = {
+  name: "",
+  lastname: "",
+  email: "",
+  phone : "",
+  date : "", 
+  street : "",
+  city : "",
+  state : "",
+  postCode : ""
+};
 
+$("#btn_continue").click(() => {
+  driverInformation.name = $("#name").val();
+  driverInformation.lastname = $("#lastname").val();
+  driverInformation.email = $("#email").val();
+  driverInformation.phone = $("#phone").val();
+  driverInformation.date = $("#date").val();
+  driverInformation.street = $("#street").val();
+  driverInformation.city = $("#city").val();
+  driverInformation.state = $("#state").val();
+  driverInformation.postCode = $("#post-code").val();
+
+  localStorage.setItem("driverInformation", driverInformation);
+});
