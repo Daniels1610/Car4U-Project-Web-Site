909ab3ed949b276255f685eaeeaf68a049b4bd40
Daniels1610 <danielagraz1610@hotmail.com>
2023-05-30 11:29:42 -0700

    Fix merge conflicts on carCheckout.js and sweetAlerts.js

--- js/carCheckout.js
+++ js/carCheckout.js
@@ -1,4 +1,4 @@
-const driverInfo = localStorage.getItem('driverInformation');
+const driverInfo = JSON.parse(sessionStorage.getItem('driverInformation'));
 // CREDIT CARD INPUT AND VALIDATION
 // Get the input field
 const cardNumberInput = document.getElementById('cc-number');
@@ -29,62 +29,101 @@ carImage2.src = `imagesCar/${selected_carId}.png`;
 // carImage.src = `imagesCar/${selectedCarID}.png`;
 
 
+// $(document).ready(function() {
+//     $('#checkout_button').submit(function(e) {
+//             e.preventDefault(); 
+//         Swal.fire({
+//             title: 'Summary Order!',
+//             allowEscapeKey: false,
+//             allowOutsideClick: false,
+//             html: '<div style="display: flex;"><div style="flex: 1;"><b>Delivery:</b><br>Tijuana<br>27, May 2023</div><div style="flex: 1;"><b>Return:</b><br>Tijuana<br>30, May 2023</div></div> <br> <b>Unlimited mileage</b> : Included <br> <b>Basic Protection Package</b>: Included <br><br> <h4 style="font-weight: bold; color: green">TOTAL PRICE:</h4> $10,000 DLLS',
+//             confirmButtonText: 'Pay now!',
+//             confirmButtonColor: 'green',
+//             showDenyButton: true,
+//             denyButtonText: 'Cancel',
+//             imageUrl: 'imagesCar/CSS2010G1.png',
+//             imageWidth: 300,
+//             imageHeight: 150,
+//             footer: '<span style="color: red"> Please check the order in detail before renting</span>'
+//         }).then((result)=>{
+//             if(result.isConfirmed){
+//                 addInfoToRents(driverInfo);
+//                 Swal.fire({
+//                     title: 'Processing your Rent',
+//                     html: 'Give us a moment, we are processing your Rent',
+//                     timer: 2000,
+//                     timerProgressBar: true,
+//                     allowOutsideClick: false,
+//                     allowEscapeKey: false,
+//                     didOpen: () => {
+//                         Swal.showLoading()
+//                         timerInterval = setInterval(() => {
+//                         }, 100)
+//                     },
+//                     willClose: () => {
+//                         clearInterval(timerInterval)
+//                     }
+//                     }).then((result) => {
+//                     if (result.dismiss === Swal.DismissReason.timer) {
+//                         addInfoToRents(driverInfo);
+//                         Swal.fire({
+//                             timerProgressBar: true,
+//                             allowOutsideClick: false,
+//                             title: 'Rent Completed',
+//                             html: 'Your Rent has been Successful<br>Thank you for Renting on Car4U!',
+//                             icon: 'success',
+//                             confirmButtonText : 'OK'
+//                         }).then((result) => {
+//                             if(result.isConfirmed) {
+//                                 window.location.href = "index.html"
+//                             }
+//                         })
+//                     } else {return;}
+//                     })
+                
+//             }
+//         })
+//         });
+//     });
+
 $(document).ready(function() {
-    $('#checkout_button').submit(function(e) {
-            e.preventDefault(); 
+    $('#payNowButton').click(function() {
+
         Swal.fire({
-            title: 'Summary Order!',
-            allowEscapeKey: false,
+            title: 'Processing your Rent',
+            html: 'Give us a moment, we are processing your Rent',
+            timer: 2000,
+            timerProgressBar: true,
             allowOutsideClick: false,
-            html: '<div style="display: flex;"><div style="flex: 1;"><b>Delivery:</b><br>Tijuana<br>27, May 2023</div><div style="flex: 1;"><b>Return:</b><br>Tijuana<br>30, May 2023</div></div> <br> <b>Unlimited mileage</b> : Included <br> <b>Basic Protection Package</b>: Included <br><br> <h4 style="font-weight: bold; color: green">TOTAL PRICE:</h4> $10,000 DLLS',
-            confirmButtonText: 'Pay now!',
-            confirmButtonColor: 'green',
-            showDenyButton: true,
-            denyButtonText: 'Cancel',
-            imageUrl: 'imagesCar/CSS2010G1.png',
-            imageWidth: 300,
-            imageHeight: 150,
-            footer: '<span style="color: red"> Please check the order in detail before renting</span>'
-        }).then((result)=>{
-            if(result.isConfirmed){
-                addInfoToRents(driverInfo);
+            allowEscapeKey: false,
+            didOpen: () => {
+                Swal.showLoading();
+                timerInterval = setInterval(() => {}, 100);
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        }).then((result) => {
+            if (result.dismiss === Swal.DismissReason.timer) {
+                addInfoToRents(driverInfo)
                 Swal.fire({
-                    title: 'Processing your Rent',
-                    html: 'Give us a moment, we are processing your Rent',
-                    timer: 2000,
                     timerProgressBar: true,
                     allowOutsideClick: false,
-                    allowEscapeKey: false,
-                    didOpen: () => {
-                        Swal.showLoading()
-                        timerInterval = setInterval(() => {
-                        }, 100)
-                    },
-                    willClose: () => {
-                        clearInterval(timerInterval)
+                    title: 'Rent Completed',
+                    html: 'Your Rent has been Successful<br>Thank you for Renting on Car4U!',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'index.html';
                     }
-                    }).then((result) => {
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                        addInfoToRents(driverInfo);
-                        Swal.fire({
-                            timerProgressBar: true,
-                            allowOutsideClick: false,
-                            title: 'Rent Completed',
-                            html: 'Your Rent has been Successful<br>Thank you for Renting on Car4U!',
-                            icon: 'success',
-                            confirmButtonText : 'OK'
-                        }).then((result) => {
-                            if(result.isConfirmed) {
-                                window.location.href = "index.html"
-                            }
-                        })
-                    } else {return;}
-                    })
-                
+                });
+            } else {
+                return;
             }
-        })
         });
     });
+});
 
 const addInfoToRents = (data) => {
     const url = `http://ec2-54-196-205-117.compute-1.amazonaws.com/rents`;
